0a81d7ba264505dd9d02f6aa675fc3f167c0dcdc
Sergejs Rakovskis <sr19062@edu.lu.lv>
2026-04-23 17:43:15 +0300

    Initial commit

---
+++ src/main/java/com/banking/api/bakalaura_darbs_platform_threads/BakalauraDarbsPlatformThreadsApplication.java
@@ -0,0 +1,13 @@
+package com.banking.api.bakalaura_darbs_platform_threads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakalauraDarbsPlatformThreadsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BakalauraDarbsPlatformThreadsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/banking/api/bakalaura_darbs_platform_threads/BakalauraDarbsPlatformThreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.api.bakalaura_darbs_platform_threads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakalauraDarbsPlatformThreadsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
