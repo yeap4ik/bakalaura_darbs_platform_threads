ffbbe1774855fb298196f56aeec79770f2b983d5
Sergejs Rakovskis <sr19062@edu.lu.lv>
2026-04-29 22:01:00 +0300

    Spring data improvements

--- src/main/java/com/banking/api/bakalaura_darbs_platform_threads/database/payment_db/dao/CustomPaymentDao.java
+++
@@ -1,9 +0,0 @@
-package com.banking.api.bakalaura_darbs_platform_threads.database.payment_db.dao;
-
-import com.banking.api.bakalaura_darbs_platform_threads.database.payment_db.entity.Payment;
-
-import java.util.UUID;
-
-public interface CustomPaymentDao {
-    Payment findPaymentById(UUID id);
-}
